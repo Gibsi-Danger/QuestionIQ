f3ed7f4c67ec189ca9900f084c1444d339cdc019
Gibsi-Danger <estivenminamina10@gmail.com>
2025-07-12 22:31:04 -0500

    Conexión correjida a la base de datos remota

--- Server.js
+++ Server.js
@@ -7,19 +7,23 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a MySQL
+
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,      // ✅ NO pongas 'localhost'
+  host: process.env.DB_HOST,         // ✅
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-
-
-db.connect(err => {
-  if (err) throw err;
-  console.log('Conectado a MySQL');
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Error de conexión a la base de datos:', err);
+    process.exit(1);
+  } else {
+    console.log('✅ Conectado a la base de datos MySQL remota');
+  }
 });
+
 
 // Ruta para obtener respuesta según la pregunta enviada
 app.get("/", (req, res) => {
